1f2f799b7f11d20299065d1b369902d87feaa57b
Jason Liu <jasonliu00@gmail.com>
2025-01-16 16:41:34 -0500

    Remove unnecessary code and comments

--- laravel-starter/source/app/Models/Reminder.php
+++ laravel-starter/source/app/Models/Reminder.php
@@ -3,15 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
 class Reminder extends Model
 {
-    // use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -60,26 +55,4 @@ class Reminder extends Model
         return Reminder::create($snakeCaseAttributes);
     }
 
-    // /**
-    //  * The attributes that should be hidden for serialization.
-    //  *
-    //  * @var array<int, string>
-    //  */
-    // protected $hidden = [
-    //     'password',
-    //     'remember_token',
-    // ];
-
-    // /**
-    //  * Get the attributes that should be cast.
-    //  *
-    //  * @return array<string, string>
-    //  */
-    // protected function casts(): array
-    // {
-    //     return [
-    //         'email_verified_at' => 'datetime',
-    //         'password' => 'hashed',
-    //     ];
-    // }
 }
