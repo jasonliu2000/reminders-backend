f1ccd32e085110928b37ef8e52769178ab4b01c5
Jason Liu <jasonliu00@gmail.com>
2025-01-17 23:32:57 -0500

    Add factory class for Reminder

---
+++ laravel-starter/source/database/factories/ReminderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reminder>
+ */
+class ReminderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->unique()->randomNumber(),
+            'user' => $this->faker->name(),
+            'text' => $this->faker->sentence(),
+            'recurrence_type' => $this->faker->randomElement(['daily', 'weekly', 'monthly']),
+            'recurrence_value' => $this->faker->randomNumber(),
+            'start_date' => $this->faker->date(),
+        ];
+    }
+}
