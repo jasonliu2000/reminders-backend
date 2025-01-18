2c8cc0ff6ce05276f8147f150639dbd4573f5726
Jason Liu <jasonliu00@gmail.com>
2025-01-17 23:35:27 -0500

    Rearrange order of endpoint routes

--- laravel-starter/source/routes/api.php
+++ laravel-starter/source/routes/api.php
@@ -8,8 +8,8 @@ Route::get('/users/{id}', [UserController::class, 'read']);
 Route::post('/users', [UserController::class, 'create']);
 
 Route::get('/reminders/search', [ReminderController::class, 'getRemindersByKeyword']);
-Route::get('/reminders', [ReminderController::class, 'getRemindersInDateRange']);
 Route::get('/reminders/{id}', [ReminderController::class, 'getById']);
+Route::get('/reminders', [ReminderController::class, 'getRemindersInDateRange']);
 Route::post('/reminders', [ReminderController::class, 'create']);
 Route::patch('/reminders/{id}', [ReminderController::class, 'patch']);
 Route::delete('/reminders/{id}', [ReminderController::class, 'delete']);
